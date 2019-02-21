d9b9c72a549e4f9b9c3bba402a944429634acc28
Binh Nguyen <xuanbinh283@gmail.com>
2019-02-21 11:48:16 -0800

    test push to hiptest

--- hiptest_push.cmd
+++ hiptest_push.cmd
@@ -1,2 +1,2 @@
 dotnet test --logger "trx;LogFileName=results.xml"
-hiptest-publisher --config-file hiptest-publisher.conf --push TestResults/results.xml --test-run-id 226958 --push-format mstest
+hiptest-publisher --config-file hiptest-publisher.conf --push TestResults/results.xml --push-format mstest
